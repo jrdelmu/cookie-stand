dd872dbad28da9a92a6a5ca8e285bea83afac47a
Jovincent Del Mundo <jovincent.delmundo@gmail.com>
2021-08-17 20:49:20 -0700

    adding constructor

--- js/app.js
+++ js/app.js
@@ -1,127 +1,157 @@
 'use strict';
 
-console.log('hello')
-// const locationsArray = [seattle, tokyo, dubai, paris, lima]
 const locationDiv = document.getElementById("locations");
 
 const hoursOpen = ['6am', '7am', '8am', '9am', '10am','11am', '12am', '1pm', '2pm', '3pm','4pm', '5pm', '6pm', '7pm']
 
-// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+Stores.storeFronts = [];
 
-const seattle = {
-  location: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  avgCust: 6.3,
-  hourlySales: [],
-// Uses a method of that object to generate a random number of customers per hour.
-  ranNumCust: function(){
-    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
-  },
-//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-  // cookiePurchase: function() {
-  //   return Math.floor(this.ranNumCust() * this.avgCust)
-  // },
-// Store the results for each location in a separate array… perhaps as a property of the object representing that location
-  currentSalePerHour: function(){
-    for(var i = 0; i < hoursOpen.length; i++){
-      var cookiePurchase = this.ranNumCust() * this.avgCust;
-      this.hourlySales.push(Math.ceil(cookiePurchase));
-    }
+function Stores(location, minCust, maxCust, avgCust, hourlySales=[]){
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCust = avgCust;
+  this.hourlySales = hourlySales
+  
+  Stores.storeFronts.push(this);
+}
+
+new Stores('Seattle', 23, 65, 6.3)
+new Stores('Tokyo', 23, 65, 6.3)
+new Stores('Dubai', 23, 65, 6.3)
+new Stores('Paris', 23, 65, 6.3)
+new Stores('Lima', 23, 65, 6.3)
+
+Stores.prototype.getRanNumCust = function(){
+  var ranNumCust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  return ranNumCust;    
+}
+Stores.hourlySales =[];
+Stores.prototype.getCurrentSalePerHour = function(){
+  for(var i = 0; i < hoursOpen.length; i++){
+    var cookiePurchase = this.ranNumCust() * this.avgCust;
+    this.hourlySales.push(Math.ceil(cookiePurchase));
   }
 }
 
-const tokyo = {
-  location: 'Tokyo',
-  minCust: 23,
-  maxCust: 65,
-  avgCust: 6.3,
-  hourlySales: [],
-// Uses a method of that object to generate a random number of customers per hour.
-  ranNumCust: function(){
-    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
-  },
-//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-  // cookiePurchase: function() {
-  //   return Math.floor(this.ranNumCust() * this.avgCust)
-  // },
-// Store the results for each location in a separate array… perhaps as a property of the object representing that location
-  currentSalePerHour: function(){
-    for(var i = 0; i < hoursOpen.length; i++){
-      var cookiePurchase = this.ranNumCust() * this.avgCust;
-      this.hourlySales.push(Math.ceil(cookiePurchase));
-    }
-  }
-}
 
-const dubai = {
-  location: 'Dubai',
-  minCust: 23,
-  maxCust: 65,
-  avgCust: 6.3,
-  hourlySales: [],
-// Uses a method of that object to generate a random number of customers per hour.
-  ranNumCust: function(){
-    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
-  },
-//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-  // cookiePurchase: function() {
-  //   return Math.floor(this.ranNumCust() * this.avgCust)
-  // },
-// Store the results for each location in a separate array… perhaps as a property of the object representing that location
-  currentSalePerHour: function(){
-    for(var i = 0; i < hoursOpen.length; i++){
-      var cookiePurchase = this.ranNumCust() * this.avgCust;
-      this.hourlySales.push(Math.ceil(cookiePurchase));
-    }
-  }
-}
 
-const paris = {
-  location: 'Paris',
-  minCust: 23,
-  maxCust: 65,
-  avgCust: 6.3,
-  hourlySales: [],
-// Uses a method of that object to generate a random number of customers per hour.
-  ranNumCust: function(){
-    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
-  },
-//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-  // cookiePurchase: function() {
-  //   return Math.floor(this.ranNumCust() * this.avgCust)
-  // },
-// Store the results for each location in a separate array… perhaps as a property of the object representing that location
-  currentSalePerHour: function(){
-    for(var i = 0; i < hoursOpen.length; i++){
-      var cookiePurchase = this.ranNumCust() * this.avgCust;
-      this.hourlySales.push(Math.ceil(cookiePurchase));
-    }
-  }
-}
+// // Stores the min/max hourly customers, and the average cookies per customer, in object properties
 
-const lima = {
-  location: 'Lima',
-  minCust: 23,
-  maxCust: 65,
-  avgCust: 6.3,
-  hourlySales: [],
-// Uses a method of that object to generate a random number of customers per hour.
-  ranNumCust: function(){
-    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
-  },
-//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-  // cookiePurchase: function() {
-  //   return Math.floor(this.ranNumCust() * this.avgCust)
-  // },
-// Store the results for each location in a separate array… perhaps as a property of the object representing that location
-  currentSalePerHour: function(){
-    for(var i = 0; i < hoursOpen.length; i++){
-      var cookiePurchase = this.ranNumCust() * this.avgCust;
-      this.hourlySales.push(Math.ceil(cookiePurchase));
-    }
-  }
-}
+// const seattle = {
+//   location: 'Seattle',
+//   minCust: 23,
+//   maxCust: 65,
+//   avgCust: 6.3,
+//   hourlySales: [],
+// // Uses a method of that object to generate a random number of customers per hour.
+//   ranNumCust: function(){
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
+//   },
+// //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+//   // cookiePurchase: function() {
+//   //   return Math.floor(this.ranNumCust() * this.avgCust)
+//   // },
+// // Store the results for each location in a separate array… perhaps as a property of the object representing that location
+//   currentSalePerHour: function(){
+//     for(var i = 0; i < hoursOpen.length; i++){
+//       var cookiePurchase = this.ranNumCust() * this.avgCust;
+//       this.hourlySales.push(Math.ceil(cookiePurchase));
+//     }
+//   }
+// }
+
+// const tokyo = {
+//   location: 'Tokyo',
+//   minCust: 23,
+//   maxCust: 65,
+//   avgCust: 6.3,
+//   hourlySales: [],
+// // Uses a method of that object to generate a random number of customers per hour.
+//   ranNumCust: function(){
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
+//   },
+// //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+//   // cookiePurchase: function() {
+//   //   return Math.floor(this.ranNumCust() * this.avgCust)
+//   // },
+// // Store the results for each location in a separate array… perhaps as a property of the object representing that location
+//   currentSalePerHour: function(){
+//     for(var i = 0; i < hoursOpen.length; i++){
+//       var cookiePurchase = this.ranNumCust() * this.avgCust;
+//       this.hourlySales.push(Math.ceil(cookiePurchase));
+//     }
+//   }
+// }
+
+// const dubai = {
+//   location: 'Dubai',
+//   minCust: 23,
+//   maxCust: 65,
+//   avgCust: 6.3,
+//   hourlySales: [],
+// // Uses a method of that object to generate a random number of customers per hour.
+//   ranNumCust: function(){
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
+//   },
+// //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+//   // cookiePurchase: function() {
+//   //   return Math.floor(this.ranNumCust() * this.avgCust)
+//   // },
+// // Store the results for each location in a separate array… perhaps as a property of the object representing that location
+//   currentSalePerHour: function(){
+//     for(var i = 0; i < hoursOpen.length; i++){
+//       var cookiePurchase = this.ranNumCust() * this.avgCust;
+//       this.hourlySales.push(Math.ceil(cookiePurchase));
+//     }
+//   }
+// }
+
+// const paris = {
+//   location: 'Paris',
+//   minCust: 23,
+//   maxCust: 65,
+//   avgCust: 6.3,
+//   hourlySales: [],
+// // Uses a method of that object to generate a random number of customers per hour.
+//   ranNumCust: function(){
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
+//   },
+// //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+//   // cookiePurchase: function() {
+//   //   return Math.floor(this.ranNumCust() * this.avgCust)
+//   // },
+// // Store the results for each location in a separate array… perhaps as a property of the object representing that location
+//   currentSalePerHour: function(){
+//     for(var i = 0; i < hoursOpen.length; i++){
+//       var cookiePurchase = this.ranNumCust() * this.avgCust;
+//       this.hourlySales.push(Math.ceil(cookiePurchase));
+//     }
+//   }
+// }
+
+// const lima = {
+//   location: 'Lima',
+//   minCust: 23,
+//   maxCust: 65,
+//   avgCust: 6.3,
+//   hourlySales: [],
+// // Uses a method of that object to generate a random number of customers per hour.
+//   ranNumCust: function(){
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);    
+//   },
+// //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+//   // cookiePurchase: function() {
+//   //   return Math.floor(this.ranNumCust() * this.avgCust)
+//   // },
+// // Store the results for each location in a separate array… perhaps as a property of the object representing that location
+//   currentSalePerHour: function(){
+//     for(var i = 0; i < hoursOpen.length; i++){
+//       var cookiePurchase = this.ranNumCust() * this.avgCust;
+//       this.hourlySales.push(Math.ceil(cookiePurchase));
+//     }
+//   }
+// }
 
 // Display the values of each array as unordered lists in the browser
 
@@ -143,28 +173,28 @@ function _makeElement(tag,parent,text){ // underscore is a helper function
   return element;
 }
 
-function renderLocation(place){
+Stores.prototype.renderLocation = function(){
   var total = 0;
   const newArticle = _makeElement ('article', locationDiv, null)
-  _makeElement('h3', newArticle, place.location)
+  _makeElement('h3', newArticle, Stores.location)
   const newUl = _makeElement('ul', newArticle, null);
   for(var i = 0; i < hoursOpen.length; i ++){
-    const text = `${hoursOpen[i]}: ${place.hourlySales[i]} cookies`;
-    total += place.hourlySales[i];
+    const text = `${hoursOpen[i]}: ${Stores.hourlySales[i]} cookies`;
+    total += Stores.hourlySales[i];
     _makeElement('li', newUl, text);
   }
   var totalsString = `Total: ${total} cookies`;
   _makeElement('li', newUl, totalsString);
 };
-
-const locationsArray = [seattle, tokyo, dubai, paris, lima]
+console.log(Stores.storeFronts)
+// const locationsArray = [seattle, tokyo, dubai, paris, lima]
 
 //loop through all locations and call these functions
 function renderAllLocations() {
-  for(var i = 0; i < locationsArray.length; i ++){
-  const currentLocation = locationsArray[i];
-  currentLocation.currentSalePerHour()
-  renderLocation(currentLocation);
+  for(var i = 0; i < Stores.storeFronts.length; i ++){
+  const currentLocation = Stores.storeFronts[i];
+  // Stores.currentLocation.currentSalePerHour()
+  currentLocation.renderLocation();
   }
 }
 
